b23344b01f09ae4199d4decf55543cd66ef2add9
Vanshdhule <154143527+Vanshdhule@users.noreply.github.com>
2023-12-18 12:54:07 +0530

    Add files via upload

---
+++ VS Python.py
@@ -0,0 +1,22 @@
+marks =int(input("enter marks: "))
+
+if marks >= 90:
+  print("Grade A")
+
+elif marks >=80 and mark <90:
+  print("Grade B")
+
+elif marks >=70 and mark <80:
+  print("Grade C")
+
+elif marks >=60 and mark <70:
+  print("Grade D")
+
+elif marks >=50 and mark <60:
+  print("Grade E")
+
+elif marks >=50:
+  print("Pass")
+
+elif marks <40:
+  print("Fail")
