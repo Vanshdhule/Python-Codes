835d339d8923af9f8d252b7b7c524336284caee6
Vanshdhule <154143527+Vanshdhule@users.noreply.github.com>
2023-12-20 19:02:44 +0530

    Add files via upload

---
+++ CGPA Calculator.py
@@ -0,0 +1,13 @@
+sub1 = int(input("enter sub1 marks : "))
+sub2 = int(input("enter sub2 marks : "))
+sub3 = int(input("enter sub3 marks : "))
+sub4 = int(input("enter sub4 marks : "))
+sub5 = int(input("enter sub5 marks : "))
+sub6 = int(input("enter sub6 marks : "))
+
+total = (sub1 + sub2 + sub3 + sub4 + sub5 + sub6)/600
+percentage = total*100
+print(percentage)
+
+CGPA = percentage/9.5
+print(CGPA)
